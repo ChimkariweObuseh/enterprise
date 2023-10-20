45cdca40044085de1357a69e0b2f39462738cd91
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-20 12:06:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleLight() {
    const element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+function thankPerson() {
+   document.body.innerHTML = '<h3>THANK YOU!</h3>';
+}
